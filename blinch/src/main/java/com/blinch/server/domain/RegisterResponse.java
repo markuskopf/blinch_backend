a8cb55d05e79d3dc19760fe63c81852a9be49bfb
Markus Kopf <kopf.markus@googlemail.com>
2016-01-21 16:30:28 -0800

    Remove old project so I can add the new one with maven.

--- blinch/src/main/java/com/blinch/server/domain/RegisterResponse.java
+++
@@ -1,9 +0,0 @@
-package com.blinch.server.domain;
-
-import com.blinch.server.domain.DefaultResponse;
-
-/**
- * Created by markuskopf on 13/01/16.
- */
-public class RegisterResponse extends DefaultResponse {
-}

59ee8ff0f507f76a7cec53a3a93fbe4b82d55c02
Markus Kopf <kopf.markus@googlemail.com>
2016-01-28 13:20:06 -0800

    Insert swagger json for api doc.

--- blinch_backend/src/main/java/com/blinch/server/controller/GroupController.java
+++ blinch_backend/src/main/java/com/blinch/server/controller/GroupController.java
@@ -14,7 +14,7 @@ import javax.validation.Valid;
  */
 
 @RestController
-@RequestMapping("/api/v1/group")
+@RequestMapping("/api/v1/groups")
 public class GroupController {
 
     private final BLIGroupService service;

1012be4ca95d51d4a61c1492b089ea700bd51d42
Markus Kopf <kopf.markus@googlemail.com>
2016-02-03 17:00:07 -0800

    Add cron job business logic

--- blinch_backend/matching-service/src/main/java/com/blinch/matcher/LotteryScheduler.java
+++ blinch_backend/matching-service/src/main/java/com/blinch/matcher/LotteryScheduler.java
@@ -1,5 +1,7 @@
 package com.blinch.matcher;
 
+import com.blinch.server.domain.event.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -15,12 +17,29 @@ public class LotteryScheduler {
 
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
+    final EventRepository eventRepository;
+
+    @Autowired
+    public LotteryScheduler(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
     @Scheduled(fixedRate = 50000)
     //@Scheduled(cron = "0/5 * * * * ?")
     public void reportCurrentTime() {
         // TODO: Check which users are checked-in and match respectively two and inform.
 
         System.out.println("LotteryScheduler component: " + dateFormat.format(new Date()));
+
+        // TODO:
+        // 1.) Check every day at 11 which groups has an event scheduled.
+        // 2.) Take those groups and check all checked-in users of this group
+        // 3.) Match them in a random way.
+            // 3. a) Consider the history of appointments
+        // 4.) Push them the information
+        // 5.) Save them in an appointment --> this is the history
+        // 6.) Delete checked-in users so start from scratch next time
+
     }
 
 

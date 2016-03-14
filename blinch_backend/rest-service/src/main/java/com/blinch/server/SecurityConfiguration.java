43bfef6bb4051b6407e66ab698b35b603aa7e10c
Markus Kopf <kopf.markus@googlemail.com>
2016-03-14 14:45:40 +0100

    Working authentication with AngularJS and CSRF.

---
+++ blinch_backend/rest-service/src/main/java/com/blinch/server/SecurityConfiguration.java
@@ -0,0 +1,98 @@
+package com.blinch.server;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+
+import javax.servlet.Filter;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.FilterChain;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
+
+import java.io.IOException;
+
+/**
+ * Created by markuskopf on 21/01/16.
+ */
+@Configuration
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().and().authorizeRequests()
+                .antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll().anyRequest()
+                .authenticated().and()
+                .logout().and()
+                .csrf().csrfTokenRepository(csrfTokenRepository()).and()
+                .addFilterAfter(csrfHeaderFilter(), CsrfFilter.class);
+    }
+
+
+
+    private Filter csrfHeaderFilter() {
+        return new OncePerRequestFilter() {
+            @Override
+            protected void doFilterInternal(HttpServletRequest request,
+                                            HttpServletResponse response, FilterChain filterChain)
+                    throws ServletException, IOException {
+
+                CsrfToken csrf = (CsrfToken) request.getAttribute(CsrfToken.class
+                        .getName());
+                if (csrf != null) {
+                    Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN");
+                    String token = csrf.getToken();
+                    if (cookie == null || token != null
+                            && !token.equals(cookie.getValue())) {
+                        cookie = new Cookie("XSRF-TOKEN", token);
+                        cookie.setPath("/");
+                        response.addCookie(cookie);
+                    }
+                }
+                filterChain.doFilter(request, response);
+            }
+        };
+    }
+
+    private CsrfTokenRepository csrfTokenRepository() {
+        HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+        repository.setHeaderName("X-XSRF-TOKEN");
+        return repository;
+    }
+
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests().
+//                anyRequest().authenticated().
+//                and().httpBasic().
+//                and().authorizeRequests().antMatchers("/").permitAll().
+//                and().csrf().disable();
+//
+//    }
+
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("mkopf").password("1qazxsw2").roles("USER")
+//                .and()
+//                .withUser("markuskopf").password("1qazxsw2").roles("ADMIN");
+//    }
+
+}
--- blinch_backend/rest-service/src/main/resources/static/js/app.js
+++ blinch_backend/rest-service/src/main/resources/static/js/app.js
@@ -1,67 +1,84 @@
-angular.module('app', [ 'ngRoute' ])
-  .config(function($routeProvider, $httpProvider) {
+
+
+angular.module('app', [ 'ngRoute' ]).config(function($routeProvider, $httpProvider) {
 
 	$routeProvider.when('/', {
 		templateUrl : 'home.html',
-		controller : 'home'
+		controller : 'home',
+		controllerAs: 'controller'
 	}).when('/login', {
 		templateUrl : 'login.html',
-		controller : 'navigation'
+		controller : 'navigation',
+		controllerAs: 'controller'
 	}).otherwise('/');
 
-    $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+	$httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-  })
-  .controller('home', function($scope, $http) {
-    $http.get('/resource/').success(function(data) {
-      $scope.greeting = data;
-    })
-  })
-  .controller('navigation',
-  function($rootScope, $scope, $http, $location) {
+}).controller('navigation',
 
-    var authenticate = function(credentials, callback) {
+		function($rootScope, $scope, $http, $location, $route) {
 
-      var headers = credentials ? {authorization : "Basic "
-          + btoa(credentials.username + ":" + credentials.password)
-      } : {};
+			var self = this;
 
-      $http.get('user', {headers : headers}).success(function(data) {
-        if (data.name) {
-          $rootScope.authenticated = true;
-        } else {
-          $rootScope.authenticated = false;
-        }
-        callback && callback();
-      }).error(function() {
-        $rootScope.authenticated = false;
-        callback && callback();
-      });
+			self.tab = function(route) {
+				return $route.current && route === $route.current.controller;
+			};
 
-    }
+			var authenticate = function(credentials, callback) {
 
-    authenticate();
-    $scope.credentials = {};
-    $scope.login = function() {
-        authenticate($scope.credentials, function() {
-          if ($rootScope.authenticated) {
-            $location.path("/");
-            $scope.error = false;
-          } else {
-            $location.path("/login");
-            $scope.error = true;
-          }
-        });
-    };
+				var headers = credentials ? {
+					authorization : "Basic "
+							+ btoa(credentials.username + ":"
+									+ credentials.password)
+				} : {};
 
-    // logout implementation
-    $scope.logout = function() {
-        $http.post('logout', {}).success(function() {
-            $rootScope.authenticated = false;
-            $location.path("/");
-            }).error(function(data) {
-                $rootScope.authenticated = false;
-            });
-    }
+				$http.get('user', {
+					headers : headers
+				}).success(function(data) {
+					if (data.name) {
+						$rootScope.authenticated = true;
+					} else {
+						$rootScope.authenticated = false;
+					}
 
-  });
+					callback && callback($rootScope.authenticated);
+				}).error(function() {
+					$rootScope.authenticated = false;
+					callback && callback(false);
+				});
+
+			}
+
+			authenticate();
+
+			self.credentials = {};
+			self.login = function() {
+				authenticate(self.credentials, function(authenticated) {
+					if (authenticated) {
+						console.log("Login succeeded")
+						$location.path("/");
+						self.error = false;
+						$rootScope.authenticated = true;
+					} else {
+						console.log("Login failed")
+						$location.path("/login");
+						self.error = true;
+						$rootScope.authenticated = false;
+					}
+				})
+			};
+
+			$scope.logout = function() {
+				$http.post('logout', {}).success(function() {
+					$rootScope.authenticated = false;
+					$location.path("/");
+				}).error(function(data) {
+				    $rootScope.authenticated = false;
+				});
+			}
+
+		}).controller('home', function($scope, $http) {
+	$http.get('/resource/').success(function(data) {
+		$scope.greeting = data;
+	})
+});

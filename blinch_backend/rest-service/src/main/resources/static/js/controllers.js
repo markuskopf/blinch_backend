480b75e4db57fded4b45c0dee25c50ffeb2cdbba
Markus Kopf <kopf.markus@googlemail.com>
2016-04-06 09:56:48 +0200

    Adjust header behaviour for sub pages.

--- blinch_backend/rest-service/src/main/resources/static/js/controllers.js
+++ blinch_backend/rest-service/src/main/resources/static/js/controllers.js
@@ -5,7 +5,27 @@ var appControllers = angular.module('appControllers', []);
 
 appControllers.controller('navigation',
 
-		function($rootScope, $scope, $http, $location, $route) {
+		function($rootScope, $scope, $http, $location, $route, sharedProperties) {
+
+		    sharedProperties.setAnimation(false);
+
+            var bar = angular.element('#bg_id');
+            bar.addClass('show');
+
+            var loginAnchor = angular.element('#loginAnchor');
+            loginAnchor.addClass('navshow');
+
+            var logoutAnchor = angular.element('#logoutAnchor');
+            logoutAnchor.addClass('navshow');
+
+            var signupAnchor = angular.element('#signupAnchor');
+            signupAnchor.addClass('navshow');
+
+            var registerAnchor = angular.element('#registerAnchor');
+            registerAnchor.addClass('navshow');
+
+            var element = angular.element('#logo');
+            element.removeClass('logoWhite');
 
 			var self = this;
 
@@ -24,6 +44,7 @@ appControllers.controller('navigation',
 				$http.get('user', {
 					headers : headers
 				}).success(function(data) {
+
 					if (data.name) {
 						$rootScope.authenticated = true;
 					} else {
@@ -48,6 +69,27 @@ appControllers.controller('navigation',
 						$location.path("/");
 						self.error = false;
 						$rootScope.authenticated = true;
+
+                        sharedProperties.setAnimation(false);
+
+                        var bar = angular.element('#bg_id');
+                        bar.addClass('show');
+
+                        var loginAnchor = angular.element('#loginAnchor');
+                        loginAnchor.addClass('navshow');
+
+                        var logoutAnchor = angular.element('#logoutAnchor');
+                        logoutAnchor.addClass('navshow');
+
+                        var signupAnchor = angular.element('#signupAnchor');
+                        signupAnchor.addClass('navshow');
+
+                        var registerAnchor = angular.element('#registerAnchor');
+                        registerAnchor.addClass('navshow');
+
+                        var element = angular.element('#logo');
+                        element.removeClass('logoWhite');
+
 					} else {
 						console.log("Login failed")
 						$location.path("/login");
@@ -61,23 +103,156 @@ appControllers.controller('navigation',
 				$http.post('logout', {}).success(function() {
 					$rootScope.authenticated = false;
 					$location.path("/");
+
 				}).error(function(data) {
 				    $rootScope.authenticated = false;
+
+				    sharedProperties.setAnimation(true);
+
+                    var bar = angular.element('#bg_id');
+                    bar.removeClass('show');
+
+                    var loginAnchor = angular.element('#loginAnchor');
+                    loginAnchor.removeClass('navshow');
+
+                    var logoutAnchor = angular.element('#logoutAnchor');
+                    logoutAnchor.removeClass('navshow');
+
+                    var signupAnchor = angular.element('#signupAnchor');
+                    signupAnchor.removeClass('navshow');
+
+                    var registerAnchor = angular.element('#registerAnchor');
+                    registerAnchor.removeClass('navshow');
+
+                    var element = angular.element('#logo');
+                    element.addClass('logoWhite');
 				});
 			}
 });
 
 
-appControllers.controller('home', function($scope, $http) {
+appControllers.controller('home',
+
+    function($rootScope, $scope, $http, $window, sharedProperties) {
+
+            if($rootScope.authenticated == false) {
+                sharedProperties.setAnimation(true);
+
+                var bar = angular.element('#bg_id');
+                            bar.removeClass('show');
+
+                            var loginAnchor = angular.element('#loginAnchor');
+                            loginAnchor.removeClass('navshow');
+
+                            var logoutAnchor = angular.element('#logoutAnchor');
+                            logoutAnchor.removeClass('navshow');
+
+                            var signupAnchor = angular.element('#signupAnchor');
+                            signupAnchor.removeClass('navshow');
+
+                            var registerAnchor = angular.element('#registerAnchor');
+                            registerAnchor.removeClass('navshow');
+
+                            var element = angular.element('#logo');
+                            element.addClass('logoWhite');
+
+            } else {
+                sharedProperties.setAnimation(false);
+                       var bg_id = angular.element('#bg_id');
+                       bg_id.addClass('show');
+
+                       var loginAnchor = angular.element('#loginAnchor');
+                       loginAnchor.addClass('navshow');
+
+                       var logoutAnchor = angular.element('#logoutAnchor');
+                       logoutAnchor.addClass('navshow');
+
+                       var signupAnchor = angular.element('#signupAnchor');
+                       signupAnchor.addClass('navshow');
+
+                       var registerAnchor = angular.element('#registerAnchor');
+                       registerAnchor.addClass('navshow');
+
+                       var element = angular.element('#logo');
+                       element.removeClass('logoWhite');
+            }
+
+
+
+            angular.element($window).bind("scroll", function() {
+
+                    if (sharedProperties.getAnimation()) {
+                        if ($window.pageYOffset > 0) {
+
+                            var bar = angular.element('#bg_id');
+                            bar.addClass('show');
+
+                            var loginAnchor = angular.element('#loginAnchor');
+                            loginAnchor.addClass('navshow');
+
+                            var logoutAnchor = angular.element('#logoutAnchor');
+                            logoutAnchor.addClass('navshow');
+
+                            var signupAnchor = angular.element('#signupAnchor');
+                            signupAnchor.addClass('navshow');
+
+                            var registerAnchor = angular.element('#registerAnchor');
+                            registerAnchor.addClass('navshow');
+
+                            var element = angular.element('#logo');
+                            element.removeClass('logoWhite');
+
+                        } else {
+                            var bar = angular.element('#bg_id');
+                            bar.removeClass('show');
+
+                            var loginAnchor = angular.element('#loginAnchor');
+                            loginAnchor.removeClass('navshow');
+
+                            var logoutAnchor = angular.element('#logoutAnchor');
+                            logoutAnchor.removeClass('navshow');
+
+                            var signupAnchor = angular.element('#signupAnchor');
+                            signupAnchor.removeClass('navshow');
+
+                            var registerAnchor = angular.element('#registerAnchor');
+                            registerAnchor.removeClass('navshow');
+
+                            var element = angular.element('#logo');
+                            element.addClass('logoWhite');
+
+                        }
+                    }
+        });
+
 	$http.get('/resource/').success(function(data) {
 		$scope.greeting = data;
 	})
-
 });
 
 appControllers.controller('register',
 
-        function($scope, $http, $location, $route) {
+        function($scope, $http, $location, $route, sharedProperties) {
+
+                sharedProperties.setAnimation(false);
+
+                var bar = angular.element('#bg_id');
+                bar.addClass('show');
+
+                var loginAnchor = angular.element('#loginAnchor');
+                loginAnchor.addClass('navshow');
+
+                var logoutAnchor = angular.element('#logoutAnchor');
+                logoutAnchor.addClass('navshow');
+
+                var signupAnchor = angular.element('#signupAnchor');
+                signupAnchor.addClass('navshow');
+
+                var registerAnchor = angular.element('#registerAnchor');
+                registerAnchor.addClass('navshow');
+
+                var element = angular.element('#logo');
+                element.removeClass('logoWhite');
 
             var self = this;
 
@@ -124,7 +299,39 @@ appControllers.controller('datePicker',
 
 appControllers.controller('signup',
 
-        function() {
-            console.log('do signup stuff');
+        function($scope, $document, sharedProperties) {
+
+                sharedProperties.setAnimation(false);
+
+                var bar = angular.element('#bg_id');
+                bar.addClass('show');
+
+                var loginAnchor = angular.element('#loginAnchor');
+                loginAnchor.addClass('navshow');
+
+                var logoutAnchor = angular.element('#logoutAnchor');
+                logoutAnchor.addClass('navshow');
+
+                var signupAnchor = angular.element('#signupAnchor');
+                signupAnchor.addClass('navshow');
+
+                var registerAnchor = angular.element('#registerAnchor');
+                registerAnchor.addClass('navshow');
+
+                var element = angular.element('#logo');
+                element.removeClass('logoWhite');
         }
 );
+
+appControllers.factory('sharedProperties', function() {
+        var animation = true;
+
+        return {
+            getAnimation: function () {
+                return animation;
+            },
+            setAnimation: function(value) {
+                animation = value;
+            }
+        };
+});
